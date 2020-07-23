a7750e95a423e674836e8bec4c7e4e9839e5fec5
Martin <martinwahlberg@icloud.com>
2020-07-23 23:48:49 +0200

    Remove built

--- built/bolt.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const bolt_1 = require("@slack/bolt");
-const boltApp = new bolt_1.App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET
-});
-exports.default = boltApp;
-//# sourceMappingURL=bolt.js.map
